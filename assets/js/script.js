921b0abfa68b4c930ab4ad7767c6cd0baa52c473
Ted J <tedmjablonowski@gmail.com>
2022-03-02 20:02:30 -0500

    Minor bug fixes

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,15 +1,29 @@
+ //Variables
+var nine = $("#9");
+var ten = $("#10");
+var eleven = $("#11");
+var twelve = $("#12");
+var thirteen = $("#13");
+var fourteen = $("#14");
+var fifthteen = $("#15");
+var sixteen = $("#16");
+var seventeen = $("#17");
+ 
  //Today's Date
  $("#currentDay").text(moment().format('dddd MMMM Do YYYY'));
 
  //Save Function
+ function plannerSave() {
   $(".saveBtn").on("click", function() {
     var planTime = $(this).parent().attr("id");
     var planText = $(this).siblings(".planText").val();
 
     localStorage.setItem(planTime, JSON.stringify(planText));
   });
+}
 
 //Load Function
+function plannerLoad() {
 $(".time-block").each(function () {
   var loadPlan = $(this).attr("id");
   var planLoad = localStorage.getItem(loadPlan);
@@ -18,6 +32,7 @@ $(".time-block").each(function () {
       $(this).children(".planText").val(planLoad);
   }
 });
+}
 
 // Change textarea color based on time of day
 function timeIsNow() {
@@ -45,5 +60,7 @@ function timeIsNow() {
   })
 };
 
+plannerLoad();
+plannerSave();
 timeIsNow();
 
