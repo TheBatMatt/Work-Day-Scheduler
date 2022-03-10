800775e7d1def3fabed58075408eef386f0d77df
Ted J <tedmjablonowski@gmail.com>
2022-03-10 07:25:15 -0500

    Refactoring code to look better, fixed slight bugs, consolidated save/load functions to look cleaner based on parent/siblings

--- assets/js/script.js
+++ assets/js/script.js
@@ -15,9 +15,12 @@ var seventeen = $("#17");
  //Save Function
  function plannerSave() {
   $(".saveBtn").on("click", function() {
+    //Targets Parent of the buttons ID
     var planTime = $(this).parent().attr("id");
+    //Targets siblings, aka other elements with the "planText" class.
     var planText = $(this).siblings(".planText").val();
 
+    //Saves to local storage.
     localStorage.setItem(planTime, JSON.stringify(planText));
   });
 }
@@ -25,9 +28,12 @@ var seventeen = $("#17");
 //Load Function
 function plannerLoad() {
 $(".time-block").each(function () {
+  //Targets id of the parent.
   var loadPlan = $(this).attr("id");
+  //Pulls from local storage.
   var planLoad = localStorage.getItem(loadPlan);
 
+  //Pulls into "planText" using the "planLoad" variable above.
   if (planLoad !== null) {
       $(this).children(".planText").val(planLoad);
   }
@@ -42,16 +48,19 @@ function timeIsNow() {
   $(".time-block").each(function() {
     var currentHour = parseInt($(this).attr("id"));
 
+    //Adds Royal Blue color to future classes
     if (currentHour > currentTime) {
       $(this).removeClass("present");
       $(this).removeClass("past");
       $(this).addClass("future");
     }
+    //Adds Red to current hour for urgency
     else if (currentHour === currentTime) {
       $(this).removeClass("future");
       $(this).removeClass("past");
       $(this).addClass("present");
     } 
+    //Grays out past hours
     else {
       $(this).removeClass("future");
       $(this).removeClass("present");
@@ -60,6 +69,7 @@ function timeIsNow() {
   })
 };
 
+//Function Calls
 plannerLoad();
 plannerSave();
 timeIsNow();
